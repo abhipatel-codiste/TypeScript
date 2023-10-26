16d22002b0c8fa9001fca58c7c7d793b1a2a616a
Ketul Solanki <ketul.s@codiste.com>
2023-10-26 17:25:51 +0530

    Typescipt types learning

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let sales = "New";
+let nnumbrt = 99;
+let numbrt = 100;
+let without;
+console.log(numbrt);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+let sales:string = "New"
+
+let nnumbrt = 99
+let numbrt: number =100
+let without;
+
+ console.log(numbrt)
